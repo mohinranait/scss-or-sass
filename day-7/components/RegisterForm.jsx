1b8de06f6a7bc9a785d758c3296234c14aa0bca7
Mohin Rana <ebrahimit49@gmail.com>
2024-02-15 01:57:22 +0600

    Deploy bug fixed

--- day-7/components/RegisterForm.jsx
+++ day-7/components/RegisterForm.jsx
@@ -21,21 +21,7 @@ export default function RegisterForm() {
     }
 
     try {
-      const resUserExists = await fetch("api/userExists", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      });
-
-      const { user } = await resUserExists.json();
-
-      if (user) {
-        setError("User already exists.");
-        return;
-      }
-
+     
       const res = await fetch("api/register", {
         method: "POST",
         headers: {
